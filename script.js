f3beeff33af5549eed1c0c20e1bfd62f647d8918
ImDr3ek <ggdiego581@gmail.com>
2026-06-09 21:57:32 -0300

    Nao tenho namorada

--- script.js
+++ script.js
@@ -19,14 +19,81 @@ setInterval(() => {
     }
 }, 5000);
 
-const btn = document.getElementById("menuBtn");
-const sidebar = document.getElementById("sidebar");
+// const btn = document.getElementById("menuBtn");
+// const sidebar = document.getElementById("sidebar");
 
-btn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
+// btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active");
+// });
 
-function menuFechar(){
-    sidebar.classList.toggle("active");
+// function menuFechar(){
+//     sidebar.classList.toggle("active");
 
+// }
+
+let casais1 = [
+
+"pepedeuerro","Mario e Peach", "Link e Zelda",
+"Sonic e Amy Rose", "Cloud Strife e Tifa Lockhart",
+"Squall Leonhart e Rinoa Heartilly", "Tidus e Yuna",
+"Geralt de Rívia e Yennefer", "Commander Shepard e Liara T'Soni",
+"Nathan Drake e Elena Fisher", "James Sunderland e Mary Shepherd-Sunderland",
+"Max Payne e Mona Sax", "Madeline e Theo",
+"Mae Borowski e Gregg Lee", "Ryu e Chun-Li",
+"Kyo Kusanagi e Yuki", "Kratos e Faye",
+"Peter Parker e Mary Jane Watson", "V e Judy Alvarez",
+"V e Panam Palmer", "Leon S. Kennedy e Ada Wong",
+"Lara Croft e Sam Nishimura", "Tracer e Emily"
+
+]
+
+let casais2 = [
+
+"pepedeuerro","Mario e Peach", "Link e Zelda",
+"Sonic e Amy Rose", "Cloud Strife e Tifa Lockhart",
+"Squall Leonhart e Rinoa Heartilly", "Tidus e Yuna",
+"Geralt de Rívia e Yennefer", "Commander Shepard e Liara T'Soni",
+"Nathan Drake e Elena Fisher", "James Sunderland e Mary Shepherd-Sunderland",
+"Max Payne e Mona Sax", "Madeline e Theo",
+"Mae Borowski e Gregg Lee", "Ryu e Chun-Li",
+"Kyo Kusanagi e Yuki", "Kratos e Faye",
+"Peter Parker e Mary Jane Watson", "V e Judy Alvarez",
+"V e Panam Palmer", "Leon S. Kennedy e Ada Wong",
+"Lara Croft e Sam Nishimura", "Tracer e Emily"
+
+]
+
+let c1 = 0
+let c2 = 0
+
+
+
+function escolha(){
+
+    
+}
+
+
+function image(){
+    
+    c1 = Number(Math.ceil(Math.random()*22))
+    document.getElementById('escolha1').innerHTML = casais1[c1]
+    
+    console.log([c1])
+    
+}
+function image2(){
+    
+    c2 = Number(Math.ceil(Math.random()*22))
+    
+    document.getElementById('escolha2').innerHTML = casais2[c2]
+    
+    console.log([c2])
+    
+    
+}
+
+function escolha(){
+
+    
 }
