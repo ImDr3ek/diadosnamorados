f387d5a2ee54d44a75c39d6f015b6daf28a4b5af
ImDr3ek <ggdiego581@gmail.com>
2026-06-11 02:16:15 -0300

    nao tenho namorada

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ setInterval(() => {
 
 let casais1 = [
 
-"pepedeuerro","Mario e Peach", "Link e Zelda",
+"Mario e Peach", "Link e Zelda",
 "Sonic e Amy Rose", "Cloud Strife e Tifa Lockhart",
 "Squall Leonhart e Rinoa Heartilly", "Tidus e Yuna",
 "Geralt de Rívia e Yennefer", "Commander Shepard e Liara T'Soni",
@@ -49,19 +49,25 @@ let casais1 = [
 
 let casais2 = [
 
-"pepedeuerro","Mario e Peach", "Link e Zelda",
+"Mario e Peach", "Link e Zelda",
 "Sonic e Amy Rose", "Cloud Strife e Tifa Lockhart",
 "Squall Leonhart e Rinoa Heartilly", "Tidus e Yuna",
 "Geralt de Rívia e Yennefer", "Commander Shepard e Liara T'Soni",
 "Nathan Drake e Elena Fisher", "James Sunderland e Mary Shepherd-Sunderland",
-"Max Payne e Mona Sax", "Madeline e Theo",
+"Max Caulfield e Chloe Price", "Madeline e Theo",
 "Mae Borowski e Gregg Lee", "Ryu e Chun-Li",
-"Kyo Kusanagi e Yuki", "Kratos e Faye",
-"Peter Parker e Mary Jane Watson", "V e Judy Alvarez",
-"V e Panam Palmer", "Leon S. Kennedy e Ada Wong",
+"Cody e May", "Kratos e Faye",
+"Ellie e Dina", "V e Judy Alvarez",
+"Deacon e Sarah", "Leon S. Kennedy e Ada Wong",
 "Lara Croft e Sam Nishimura", "Tracer e Emily"
 
 ]
+
+let imagenes = [
+"0.png","1.png","2.png","3.png","4.png",
+"5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png",
+"13.png","14.png","15.png","16.png","17.png","18.png","19.png","20.png","21.png"
+];
 
 let c1 = 0
 let c2 = 0
@@ -76,24 +82,28 @@ function escolha(){
 
 function image(){
     
-    c1 = Number(Math.ceil(Math.random()*22))
+    c1 = Number(Math.floor(Math.random()*casais1.length))
     document.getElementById('escolha1').innerHTML = casais1[c1]
-    
+    document.getElementById('img1').src = `${c1}.png`;
     console.log([c1])
+    casais1.splice(c1,1);
+    imagenes.splice(c1,1);
     
 }
 function image2(){
-    
-    c2 = Number(Math.ceil(Math.random()*22))
-    
+    c2 = Number(Math.floor(Math.random()*casais2.length))
     document.getElementById('escolha2').innerHTML = casais2[c2]
-    
+    document.getElementById('img2').src = `${c2}.png`;
     console.log([c2])
+    casais2.splice(c2,1);
+    imagenes.splice(c2,1);
     
     
 }
 
-function escolha(){
+
+
+// function escolha(){
 
     
-}
+// }
