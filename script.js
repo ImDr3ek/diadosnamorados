10bf3795b4a272c2d6ce54debda96098ea338118
DIEGO CUBIELLES GUIJARRO <diego_guijarro@estudante.sesisenai.org.br>
2026-07-17 21:56:38 -0300

    actualizacion2

--- script.js
+++ script.js
@@ -53,12 +53,28 @@ const casais = [
   { casal: "Sara e Daecon", img: "18.png" },
   { casal: "Leon S. Kennedy e Ada Wong", img: "19.png" },
   { casal: "Lara Croft e Sam Nishimura", img: "20.png" },
-  { casal: "Tracer e Emily", img: "21.png" }
+  { casal: "Tracer e Emily", img: "21.png" },
+  { casal: "John Marston Abigail Marston", img: "john & abigail marston.png" },
+  { casal: "Master Chief e Cortana", img: "master Chief.png" },
+  { casal: "Geralt de Rívia e Triss Merigold", img: "Geralrt.png" },
+  { casal: "Arthur Morgan e Mary Linton", img: "Arthur Morgan & Mary Linton.png" },
+  { casal: "Pac-Man e Ms. Pac-Man", img: "Nurse Ms_ Pac-Man Merchandising, 1980s.png" },
+  { casal: "Zagreus e Megaera", img: "magarena.png" },
+  { casal: "Joel e Tess", img: "Joel and Tess.png" },
+  { casal: "Sora e Kairi", img: "sora.png" },
+  { casal: "Zagreus e Thanatos", img: "Viadagem.png" },
+  { casal: "Toriel e Asgore Dreemurr", img: "Toriel.png" },
+  { casal: "Alphys e Undyne", img: "undyne.png" },
+  { casal: "Clementine e Louis", img: "clementine.png" },
+  { casal: "Jason Brody e Liza Snow", img: "vaas montenegro icons_ far cry 3 icons_.png" },
+  { casal: "Isaac Clarke e Nicole Brennan", img: "Isaac.png" },
+  { casal: "Solid Snake e Meryl Silverburgh", img: "SolidS.png" },
+  { casal: "Joker e Makoto Niijima", img: "Joker.png" },
 ]
 
 
-const opt1 = document.getElementById('opt1')
-const opt2 = document.getElementById('opt2')
+// const opt1 = document.getElementById('opt1')
+// const opt2 = document.getElementById('opt2')
 const reinicioBtn = document.getElementById('reinicio')
 
 
@@ -68,9 +84,9 @@ function image(){
     
     document.getElementById('img1').src = casais[sortido].img
     document.getElementById("teste1").innerHTML = casais[sortido].casal
-    console.log("el sorteado " + sortido)
+    console.log("el sorteado ", sortido)
     let teste = casais.splice(sortido, 1)
-    console.log("el eliminado " + teste)
+    console.log("el eliminado ", teste)
     
     
     if(casais.length === 0){
@@ -87,7 +103,8 @@ function image(){
 function image2(){
     
     let sortido = (Math.floor(Math.random()*casais.length))
-      document.getElementById('img2').src = casais[sortido].img
+
+    document.getElementById('img2').src = casais[sortido].img
     document.getElementById("teste2").innerHTML = casais[sortido].casal
     console.log("el sorteado " + sortido)
     let teste = casais.splice(sortido, 1)
@@ -95,7 +112,7 @@ function image2(){
  
     
     if(casais.length === 0){
-        opt1.classList.add('hide')
+        opt2.classList.add('hide')
         document.getElementById("teste2").innerHTML = ""
         reinicioBtn.classList.add('visible')
 
